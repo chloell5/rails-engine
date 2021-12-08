7419591793de45e3063faa34dfe14ebaee0a33cf
ChloeLL <chloell@protonmail.ch>
2021-12-08 14:10:25 -0700

    Add test for item merchant info

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -109,4 +109,21 @@ describe 'Item API' do
     expect(response).to be_successful
     expect(Item.count).to eq(0)
   end
+
+  it 'gets merchant data for an item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    get api_v1_item_merchant_path(item)
+    expect(response).to be_successful
+    binding.pry
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to be_a(Hash)
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data]).to have_key(:type)
+    expect(merchant[:data][:type]).to eq('merchant')
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a String
+  end
 end
