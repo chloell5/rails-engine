b45d5894da4ca92ee74c367d84d081a87c7d2c48
ChloeLL <chloell@protonmail.ch>
2021-12-08 13:58:50 -0700

    Add merchant item view functionality

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -36,4 +36,28 @@ describe 'Merchant API' do
     expect(merchants[:data][:attributes]).to have_key(:name)
     expect(merchants[:data][:attributes][:name]).to be_a String
   end
+
+  it 'gets a merchants items' do
+    merchant = create(:merchant)
+    create_list(:item, 10, merchant_id: merchant.id)
+
+    get api_v1_merchant_items_path(merchant)
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to be_a(Hash)
+    expect(items[:data]).to be_an(Array)
+    expect(items[:data].count).to eq(10)
+    expect(items[:data].first).to have_key(:id)
+    expect(items[:data].first).to have_key(:type)
+    expect(items[:data].first[:type]).to eq('item')
+    expect(items[:data].first[:attributes]).to have_key(:name)
+    expect(items[:data].first[:attributes]).to have_key(:description)
+    expect(items[:data].first[:attributes]).to have_key(:unit_price)
+    expect(items[:data].first[:attributes][:name]).to be_a String
+    expect(items[:data].first[:attributes][:description]).to be_a String
+    expect(items[:data].first[:attributes][:unit_price]).to be_a Float
+  end
 end
