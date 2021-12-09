092d60ad0a9aa5cd6040787f7bbf45d8ffa5f653
ChloeLL <chloell@protonmail.ch>
2021-12-09 16:02:44 -0700

    Adjust exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,9 +1,9 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
-  
+
   included do
     rescue_from ActionController::BadRequest do |e|
-      render json: { errors: 'Invalid parameters' }, status: 400
+      render json: { errors: 'Bad Request' }, status: 400
     end
   end
 end
